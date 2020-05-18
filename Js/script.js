6aa064c0ba4270848ae9c5b6e321ace66b32581f
suzie254 <suzieakinyi08@gmail.com>
2020-05-18 22:49:03 +0300

    Created a responsive toppings menu with different types of delicacies

--- Js/script.js
+++ Js/script.js
@@ -8,3 +8,13 @@ $(document).ready(function(){
   
       $("#addtopping").click(function(event) {
         event.preventDefault();
+        $(".toppings").append( '<select id="topping1" class="input">'+
+        '<option value="" hidden disabled selected value>Pizza topping....</option>'+
+        '<option value="300">Pepperoni</option>'+
+        '<option value="2500">Mushrooms</option>'+
+        '<option value="200">Sausage</option>'+
+        '<option value="200">Onions</option>'+
+        '<option value="200">Bacons</option>'+
+        '<option value="200">Extra Cheese</option>'+
+    '</select>');
+});
