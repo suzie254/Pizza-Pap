4f93bc8c669256905e424cc9f54cc8d164ad91d5
suzie254 <suzieakinyi08@gmail.com>
2020-05-19 12:58:02 +0300

    created a function for the totals of the delivering services

--- Js/script.js
+++ Js/script.js
@@ -43,3 +43,31 @@ $('#addcart').click(function(event){
     let cost = size + crust+ costofToppings
     var total = cost*quantity;
     var grandTotal = 0;
+     
+
+    $('table#tablecart').append('<tr>'+'<td>'+ $("#size option:selected").text()+'</td>'+ '<td id="quantity">'+quantity+'</td>' + '<td id="price">'+cost+'</td>' +'<td id="total">'+total+'</td>'+'</tr>');
+
+    $(".checkout").click(function() {
+      $(".checkout").hide();
+      $(".delivery").show();
+      $("#yes").hide();
+      $("#no").hide();
+      $("#totalcost").show();
+      $("#delivercost").show();
+      $(".yes-button").show();
+      $(".no-button").show();
+      $(".address").hide();
+
+      grandTotal = grandTotal + total;
+      $("p span").html(grandTotal);
+    });   
+    $(".yes-button").click(function() {
+        $("#delivercost").hide();
+        $("#no").hide();
+        $("#yes").hide();
+        $(".address").show();
+        $(".yes-button").hide();
+        $(".no-button").hide();
+        $("#totalcost span").html(grandTotal + 300);
+      });
+  
